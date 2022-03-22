72216407a37e736acfd1a69b4ef9453ee4426f5e
SEBLEWONGEL GIRMA BAYU <99327556+SEBLEWONGELG@users.noreply.github.com>
2022-03-22 03:18:44 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,40 +1,24 @@
+#include<stdio.h>
 #include "main.h"
-
 /**
- * string_length - finds the length of a string.
- * Return: length of c.
- * @pointer: pointer.
- */
-int string_length(char *pointer)
-{
-	int c = 0;
-
-	while (*(pointer + c) != '\0')
-	{
-		c++;
-	}
-	return (c);
-}
-/**
- * *_strcpy - copies the string pointed to by src.
- *
- * @dest: pointer to the buffer.
- * @src: pointer to the source string.
- * Return: char
+ *_strcpy - update value.
+ *@dest: value to be evaluate.
+ *@src: value to be evaluate.
+ *Return: not.
  */
 char *_strcpy(char *dest, char *src)
 {
-	int n;
-	int i;
+	int l = 0;
+	int x = 0;
 
-	n = string_length(src);
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	while (*(src + l) != '\0')
 	{
-		dest[i] = src[i];
+		l++;
 	}
-	for (; i < n; i++)
+	for ( ; x < l ; x++)
 	{
-		dest[i] = \0;
+		dest[x] = src[x];
 	}
+	dest[l] = '\0';
 	return (dest);
 }
