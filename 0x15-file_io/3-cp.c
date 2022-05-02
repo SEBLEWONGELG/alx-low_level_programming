d53b048e33333d81fc63d40df907d7d630759bfd
SEBLEWONGEL GIRMA BAYU <99327556+SEBLEWONGELG@users.noreply.github.com>
2022-05-02 17:23:13 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,86 +1,50 @@
 #include "main.h"
-void cant_write(char *s);
-void cant_read(char *s);
 /**
-* main - entry point!
-* @s: string to read/writey
-*
-* Description: cp file_from file_to
-* Return: Always 0
-*/
+  * main - copy one file to another.
+  * @argc: argument count.
+  * @argv: argument vector.
+  *
+  * Return: 0.
+  */
 int main(int argc, char *argv[])
 {
-	char *buff[1024];
-	int fd1, fd2;
-	ssize_t rd, wr;
+	int file1, file2, size;
+	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
-	if (argv[1] == NULL)
+	file1 =  open(argv[1], O_RDONLY, 0);
+	if (file1 == -1)
 	{
-		cant_read(argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 == -1)
-		cant_read(argv[1]);
-
-	fd2 = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd2 == -1)
-		cant_write(argv[2]);
-
-	rd = read(fd1, buff, 1024);
-	if (rd == -1)
-		cant_read(argv[1]);
-
-	wr = write(fd2, buff, rd);
-	if (wr == -1)
-		cant_write(argv[2]);
-	while (rd == 1024)
+	file2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (file2 == 1)
 	{
-		rd = read(fd1, buff, 1024);
-		if (rd == -1)
-			cant_read(argv[1]);
-		wr = write(fd2, buff, rd);
-		if (wr == 1)
-			cant_write(argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-	if (close(fd1) < 0)
+	while ((size = read(file1, buf, 1024)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-		exit(100);
+		if (write(file2, buf, size) != size)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		}
 	}
-	if (close(fd2) < 0)
+	if (size == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(file1) != 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file1), exit(100);
+	}
+	if (close(file2) != 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file2), exit(100);
 	}
 	return (0);
-}
-/**
-* cant_read - function
-* @s: string to read
-*
-* Description: error function if unable to read
-* Return: 0
-*/
-void cant_read(char *s)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
-	exit(98);
-}
-/**
-* cant_write - function
-* @s: string to write
-*
-* Description: error func if unable to write
-* Return: 0
-*/
-void cant_write(char *s)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
-	exit(99);
 }
